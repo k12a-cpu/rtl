d1f3a303b2c834052a8c4e48264f169e270a1ee6
Kier Davis <kierdavis@gmail.com>
2016-08-23 17:35:08 +0100

    Minor changes to run_testbench.sh

--- run_testbench.sh
+++ run_testbench.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eux
 
-iverilog -o k12a_tb.vvp -g2005-sv -I. -Ik12a -s k12a_tb k12a/*.sv ic/*.sv
-./k12a_tb.vvp
+iverilog -tvvp -ok12a_tb.vvp -g2005-sv -I. -Ik12a -s k12a_tb k12a/*.sv ic/*.sv
+vvp ./k12a_tb.vvp
