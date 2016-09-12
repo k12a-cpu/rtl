9460919023c999c9397440806f921f91fb8316c2
Kier Davis <kierdavis@gmail.com>
2016-09-12 19:27:50 +0100

    Add -Wall to iverilog invocation to catch all warnings

--- run_testbench.sh
+++ run_testbench.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eux
 
-iverilog -tvvp -ok12a_tb.vvp -g2005-sv -I. -Ik12a -s k12a_tb k12a/*.sv ic/*.sv
+iverilog -Wall -tvvp -ok12a_tb.vvp -g2005-sv -I. -Ik12a -s k12a_tb k12a/*.sv ic/*.sv
 vvp ./k12a_tb.vvp
